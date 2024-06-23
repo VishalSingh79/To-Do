2de613d6b0cb46e44a22dbfc931363f5ef82225a
VishalSingh79 <us55547660@gmail.com>
2024-06-23 12:26:58 +0530

    edit-option added

--- src/components/AllTodo.jsx
+++ src/components/AllTodo.jsx
@@ -4,7 +4,7 @@ import { useSelector,useDispatch } from 'react-redux'
 import { removeTodo,completedTodo,updateTodo,updationTodo} from '../redux/todo/todoSlice'
 import { FaRegEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-import { TiTick } from "react-icons/ti";
+
 function AllTodo() {
    const [completed,setCompleted]=useState(false);
    const [input,setInput]=useState('');
@@ -28,6 +28,10 @@ function AllTodo() {
     {
       dispatch(updationTodo(todo));
     }
+    function handleEditClick(todo) {
+      setInput(todo.text); 
+      updationTodohandler(todo);
+    }
   
   return (
     <div className='todo-data'>
@@ -47,7 +51,7 @@ function AllTodo() {
                   todo.update ?
                   <div className='task-edit-info'>
                    <div> 
-                      <input type='text'  id='todo' name='todo' autoFocus  placeholder={todo.text}  onChange={(e)=>setInput(e.target.value)} value={input} className='update-text'/> 
+                      <input type='text'  id='todo' name='todo' autoFocus  onChange={(e)=>setInput(e.target.value)} value={input} className='update-text'/> 
                    </div>
                    <div  onClick={()=>updateHandler(todo.id,input)} className='save-todo'>
                       Save
@@ -64,7 +68,7 @@ function AllTodo() {
                   className='task-status'
                   onClick={() => completeHandler(todo)}
                   style={{
-                    // width: '85%',
+                
                     height: '100%',
                     backgroundColor: todo.completed ? 'green' : 'rgb(154,193,189)',
                     borderRadius: '5px',
@@ -72,9 +76,10 @@ function AllTodo() {
                     cursor: 'pointer'
                   }}
                 >
+               
                   {todo.completed ? 'Completed' : 'Pending'}
                 </div>
-                  <div className='task-edit' onClick={()=>updationTodohandler(todo)}>
+                  <div className='task-edit'  onClick={() => handleEditClick(todo)}>
                         <FaRegEdit style={{color:"green",fontSize:'larger'}}/>
                   </div>
                   <div className='task-delete' onClick={()=>dispatch(removeTodo(todo))}>
